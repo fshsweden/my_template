cf5927c0a61a5f935ec285857fa556f2d0b6ed0a
Peter Andersson <fshsweden@hotmail.com>
2016-01-18 00:13:46 +0100

    wip

--- server/methods.js
+++ server/methods.js
@@ -48,14 +48,15 @@ Meteor.methods({
     Tasks.update(taskId, { $set: { private: setToPrivate } });
   },
 
-
-
   updateProductPrice: function(product, fields) {
 
     console.log("arg1:" + JSON.stringify(product));
     // console.log("arg2:" + JSON.stringify(fields));
 
+    // var o = JSON.parse("{\"askqty\":\"4\",\"product\":\"ABBN\",\"lastqty\":\"6\",\"last\":\"5\",\"bidqty\":\"77\",\"ask\":\"3\",\"bid\":\"2\"}");
     var o = JSON.parse(product);
+    console.log(JSON.stringify(o));
+
     Prices.upsert({product: o.product}, {$set: o});
   }
 
